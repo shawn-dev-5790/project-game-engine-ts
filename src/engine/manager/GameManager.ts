5ee8557fc89ae995242b4e802edd1d71c17d91c1
shawn-dev-5790 <shawn.dev.5790@gmail.com>
2024-01-07 22:27:58 +0900

    feat: :art: 게임 메니저 클래스 주석 추가

--- src/engine/manager/GameManager.ts
+++ src/engine/manager/GameManager.ts
@@ -1,5 +1,9 @@
 import GameLoop from "../GameLoop";
 
+/**
+ * 게임 매니저 클래스입니다.
+ * 게임 루프를 초기화하고 게임을 시작 또는 중지하는 기능을 제공합니다.
+ */
 class GameManager {
   private static readonly instance: GameManager = new GameManager();
   private gameLoop: GameLoop | null = null;
@@ -10,18 +14,34 @@ class GameManager {
     }
   }
 
+  /**
+   * GameManager의 인스턴스를 반환합니다.
+   * @returns {GameManager} GameManager의 인스턴스
+   */
   public static getInstance(): GameManager {
     return GameManager.instance;
   }
 
+  /**
+   * 새로운 게임 루프를 초기화합니다.
+   * @param {HTMLCanvasElement} canvas - 게임 루프에 사용될 캔버스 엘리먼트
+   * @param {number} fps - 게임 루프의 초당 프레임 수
+   * @param {number} speed - 게임 루프의 진행 속도
+   */
   public init(canvas: HTMLCanvasElement, fps: number, speed: number) {
     this.gameLoop = new GameLoop(canvas, fps, speed);
   }
 
+  /**
+   * 게임 루프를 시작합니다.
+   */
   public start() {
     this.gameLoop?.start();
   }
 
+  /**
+   * 게임 루프를 중지합니다.
+   */
   public stop() {
     this.gameLoop?.stop();
   }
